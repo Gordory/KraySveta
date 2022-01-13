76bcbe7cc622e2d874a365907bfe618c49b730d9
n.ilinykh <n.ilinykh@skbkontur.ru>
2022-01-14 02:11:54 +0700

    Added RaidParser implementation and manual tests

---
+++ KraySveta.Tests/External.ThatsMyBis/RaidParserTests.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using System.Threading.Tasks;
+using FluentAssertions;
+using KraySveta.External.ThatsMyBis.Parsers;
+using NUnit.Framework;
+
+namespace KraySveta.Tests.External.ThatsMyBis
+{
+    [TestFixture]
+    public class RaidParserTests
+    {
+        private RaidParser _raidParser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _raidParser = new RaidParser();
+        }
+
+        [TestCase("raid.16111.html")]
+        [TestCase("raid.27338.html")]
+        [TestCase("test-raid.24133.html")]
+        public async Task ParseAsync_Raid_ShouldReturnCorrectRaid(string raidFilename)
+        {
+            await using var stream = File.OpenRead($"./External.ThatsMyBis/Resources/{raidFilename}");
+            using var streamReader = new StreamReader(stream);
+
+            var raid = await _raidParser.ParseAsync(streamReader);
+            raid.Should().NotBeNull();
+        }
+    }
+}
