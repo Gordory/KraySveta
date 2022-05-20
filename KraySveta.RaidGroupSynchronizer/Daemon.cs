f5f97db84dc61069c818a9ca2e5e4b2e2c500e84
n.ilinykh <n.ilinykh@skbkontur.ru>
2022-05-20 23:32:07 +0700

    Add check if raid group already synchronized. Fixed main rain groups sync

--- KraySveta.RaidGroupSynchronizer/Daemon.cs
+++ KraySveta.RaidGroupSynchronizer/Daemon.cs
@@ -11,119 +11,133 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MoreLinq;
 
-namespace KraySveta.RaidGroupSynchronizer
+namespace KraySveta.RaidGroupSynchronizer;
+
+public class Daemon : IHostedService
 {
-    public class Daemon : IHostedService
+    private readonly ISyncRaidGroupsFactory _syncRaidGroupsFactory;
+    private readonly IThatsMyBisClient _thatsMyBisClient;
+    private readonly IOptions<DaemonConfig> _daemonConfig;
+    private readonly ILogger<Daemon> _logger;
+
+    public Daemon(
+        ISyncRaidGroupsFactory syncRaidGroupsFactory,
+        IThatsMyBisClient thatsMyBisClient,
+        IOptions<DaemonConfig> daemonConfig,
+        ILogger<Daemon> logger)
     {
-        private readonly ISyncRaidGroupsFactory _syncRaidGroupsFactory;
-        private readonly IThatsMyBisClient _thatsMyBisClient;
-        private readonly IOptions<DaemonConfig> _daemonConfig;
-        private readonly ILogger<Daemon> _logger;
+        _syncRaidGroupsFactory = syncRaidGroupsFactory;
+        _thatsMyBisClient = thatsMyBisClient;
+        _daemonConfig = daemonConfig;
+        _logger = logger;
+    }
 
-        public Daemon(
-            ISyncRaidGroupsFactory syncRaidGroupsFactory,
-            IThatsMyBisClient thatsMyBisClient,
-            IOptions<DaemonConfig> daemonConfig,
-            ILogger<Daemon> logger)
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
         {
-            _syncRaidGroupsFactory = syncRaidGroupsFactory;
-            _thatsMyBisClient = thatsMyBisClient;
-            _daemonConfig = daemonConfig;
-            _logger = logger;
-        }
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            while (!cancellationToken.IsCancellationRequested)
+            try
             {
-                try
+                await RunInternalAsync(cancellationToken);
+                _logger.LogInformation("Sync successfully done");
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e, "Exception was catch while syncing raid groups");
+            }
+
+            var period = _daemonConfig.Value.Period;
+            _logger.LogInformation("Next sync after {Period} in {Date} (UTC)", period, DateTime.UtcNow + period);
+            await Task.Delay(period, cancellationToken);
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private async Task RunInternalAsync(CancellationToken cancellationToken)
+    {
+        await _thatsMyBisClient.SyncRolesAsync();
+
+        var mainRaidGroupRoleIds = _daemonConfig.Value.MainRaidGroupRoleIds;
+        var generalRaidGroupRoleIds = _daemonConfig.Value.GeneralRaidGroupRoleIds;
+
+        var raidGroups = await _syncRaidGroupsFactory.CreateAsync();
+
+        var syncRaidGroups = raidGroups
+            .Where(x => 
+                mainRaidGroupRoleIds.Contains(x.TmbRole.DiscordRoleId) ||
+                generalRaidGroupRoleIds.Contains(x.TmbRole.DiscordRoleId))
+            .ToArray();
+
+        foreach (var syncRaidGroup in syncRaidGroups)
+        {
+            using (_logger.BeginScope("[{Name}]", syncRaidGroup.TmbRaidGroup.Name))
+            {
+                var isMainGroup = mainRaidGroupRoleIds.Contains(syncRaidGroup.TmbRole.DiscordRoleId);
+                var characters = syncRaidGroup.SyncUsers
+                    .Select(SelectCharacter)
+                    .Where(x => x != null)
+                    .ToArray();
+
+                switch (isMainGroup)
                 {
-                    await RunInternalAsync(cancellationToken);
-                    _logger.LogInformation("Sync successfully done");
+                    case true when characters.All(character => character!.RaidGroupId == syncRaidGroup.TmbRaidGroup.Id):
+                        _logger.LogInformation("Main Raid Group '{Name}' already synchronized", syncRaidGroup.TmbRaidGroup.Name);
+                        continue;
+                    case false when characters.All(character => character!.SecondaryRaidGroups.Any(raidGroup => raidGroup.Id == syncRaidGroup.TmbRaidGroup.Id)):
+                        _logger.LogInformation("General Raid Group '{Name}' already synchronized", syncRaidGroup.TmbRaidGroup.Name);
+                        continue;
                 }
-                catch (Exception e)
-                {
-                    _logger.LogCritical(e, "Exception was catch while syncing raid groups");
-                }
 
-                var period = _daemonConfig.Value.Period;
-                _logger.LogInformation("Next sync after {Period} in {Date} (UTC)", period, DateTime.UtcNow + period);
-                await Task.Delay(period, cancellationToken);
+                await _thatsMyBisClient.UpdateRaidGroupAsync(syncRaidGroup.TmbRaidGroup, characters!, isMainGroup);
+
+                if (isMainGroup)
+                    _logger.LogInformation("Main Raid Group '{Name}' successfully synchronized", syncRaidGroup.TmbRaidGroup.Name);
+                else
+                    _logger.LogInformation("General Raid Group '{Name}' successfully synchronized", syncRaidGroup.TmbRaidGroup.Name);
             }
         }
+    }
 
-        public Task StopAsync(CancellationToken cancellationToken)
+    private Character? SelectCharacter(SyncUser syncUser)
+    {
+        var (alts, mains) = MoreEnumerable.Partition(syncUser.TmbCharacters, x => x.IsAlt);
+
+        var mainsArray = mains.ToArray();
+        var altsArray = alts.ToArray();
+
+        if (mainsArray.Length == 0 && altsArray.Length == 0)
         {
-            return Task.CompletedTask;
+            _logger.LogWarning(
+                "Found no characters for '{Username}' with id: '{Id}'",
+                syncUser.DiscordUser.Username, syncUser.DiscordUser.Id);
+            return null;
         }
 
-        private async Task RunInternalAsync(CancellationToken cancellationToken)
+        var characters = mainsArray;
+        if (mainsArray.Length == 0)
         {
-            await _thatsMyBisClient.SyncRolesAsync();
-
-            var mainRaidGroupRoleIds = _daemonConfig.Value.MainRaidGroupRoleIds;
-            var generalRaidGroupRoleIds = _daemonConfig.Value.GeneralRaidGroupRoleIds;
-
-            var raidGroups = await _syncRaidGroupsFactory.CreateAsync();
-
-            var syncRaidGroups = raidGroups
-                .Where(x => generalRaidGroupRoleIds.Contains(x.TmbRole.DiscordRoleId));
-
-            foreach (var syncRaidGroup in syncRaidGroups)
-            {
-                using (_logger.BeginScope(syncRaidGroup.TmbRaidGroup.Name))
-                {
-                    var isMainGroup = mainRaidGroupRoleIds.Contains(syncRaidGroup.TmbRole.DiscordRoleId);
-                    var characters = syncRaidGroup.SyncUsers
-                        .Select(SelectCharacter)
-                        .Where(x => x != null)
-                        .ToArray();
-
-                    await _thatsMyBisClient.UpdateRaidGroupAsync(syncRaidGroup.TmbRaidGroup, characters!, isMainGroup);
-                }
-            }
+            _logger.LogWarning(
+                "Not found main characters for '{Username}' with id: '{Id}'",
+                syncUser.DiscordUser.Username, syncUser.DiscordUser.Id);
+            characters = altsArray;
         }
 
-        private Character? SelectCharacter(SyncUser syncUser)
-        {
-            var (alts, mains) = MoreEnumerable.Partition(syncUser.TmbCharacters, x => x.IsAlt);
+        var selected = Enumerable.MaxBy(
+            characters, 
+            x => GetNickname(syncUser.DiscordUser).Contains(x.Name) ? x.Name.Length : 0);
 
-            var mainsArray = mains.ToArray();
-            var altsArray = alts.ToArray();
+        _logger.LogDebug(
+            "Selected '{Character}' character for '{Username}' with id: '{Id}'", 
+            selected!.Name, syncUser.DiscordUser.Username, syncUser.DiscordUser.Id);
+        return selected;
+    }
 
-            var characters = mainsArray;
-            if (characters.Length == 0)
-            {
-                _logger.LogWarning(
-                    "Not found main characters for '{Username}' with id: '{Id}'",
-                    syncUser.DiscordUser.Username, syncUser.DiscordUser.Id);
-                characters = altsArray;
-            }
-
-            if (characters.Length == 0)
-            {
-                _logger.LogWarning(
-                    "Found no characters for '{Username}' with id: '{Id}'",
-                    syncUser.DiscordUser.Username, syncUser.DiscordUser.Id);
-                return null;
-            }
-
-            var selected = characters
-                               .OrderByDescending(x =>
-                                   GetNickname(syncUser.DiscordUser).Contains(x.Name) ? x.Name.Length : 0)
-                               .FirstOrDefault()
-                           ?? mainsArray.FirstOrDefault() 
-                           ?? altsArray.FirstOrDefault();
-
-            _logger.LogDebug(
-                "Selected '{Character}' character for '{Username}' with id: '{Id}'", 
-                selected!.Name, syncUser.DiscordUser.Username, syncUser.DiscordUser.Id);
-            return selected;
-        }
-
-        private static string GetNickname(IGuildUser discordUser)
-        {
-            return discordUser.Nickname ?? discordUser.Username;
-        }
+    private static string GetNickname(IGuildUser discordUser)
+    {
+        return discordUser.Nickname ?? discordUser.Username;
     }
 }
